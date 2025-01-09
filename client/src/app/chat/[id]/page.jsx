630b2ab2b7673eedd5a7fb28a3bbe93ce07ac6da
Soumyadip Dey <soumyadipdey802@gmail.com>
2025-01-09 10:20:58 +0530

    chat screen is getting ready

--- client/src/app/chat/[id]/page.jsx
+++ client/src/app/chat/[id]/page.jsx
@@ -1,6 +1,38 @@
-export default function Page({params}){
-    console.log(params);
-    return (
-        <div>hi for soumya</div>
-    )
+import Image from "next/image";
+import { MdOutlineEmojiEmotions } from "react-icons/md";
+import { IoAttachOutline } from "react-icons/io5";
+import { AiOutlineAudio } from "react-icons/ai";
+
+export default function Page({ params }) {
+  console.log(params);
+  return (
+    <section>
+      <header className="flex items-center gap-4 p-4">
+        <Image
+        src={"/images/profile-logo.png"} 
+        width={40}
+        height={40}
+        className="rounded-full"
+        alt="Display profile"
+        />
+        <div>
+          <h1>Name</h1>
+          <p>Message yourself</p>
+        </div>
+      </header>
+      <main className="min-h-screen"></main>
+      <footer className="flex justify-center">
+        <div className="flex basis-[20%]">
+          <MdOutlineEmojiEmotions />
+          <IoAttachOutline />
+        </div>
+        <div className="basis-[60%]">
+            <input type="text" name="message" />
+        </div>
+        <div className="basis-[20%]">
+            <AiOutlineAudio/>
+        </div>
+      </footer>
+    </section>
+  );
 }
--- client/src/app/chat/layout.jsx
+++ client/src/app/chat/layout.jsx
@@ -1,10 +1,13 @@
+"use client"
 import { FaSearch } from "react-icons/fa";
 import { MdGroupAdd, MdGroups } from "react-icons/md";
 import { IoMdNotifications } from "react-icons/io";
 import { MdLogout } from "react-icons/md";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function ChatLayout({ children }) {
+  const router = useRouter();
   return (
     <div>
       <nav className="flex justify-between p-6 shadow-xl">
@@ -33,7 +36,7 @@ export default function ChatLayout({ children }) {
         <div className="basis-[35%] pr-2 border-r-2 border-slate-700">
           <h2>Active Chats</h2>
           <ul className="my-4">
-            <li className="flex gap-4 my-2 p-2 rounded-md items-center hover:bg-slate-900">
+            <li className="flex gap-4 my-2 p-2 rounded-md items-center hover:bg-slate-900" onClick={()=> router.push("/chat/soumya")}>
               <div>
                 <Image
                   src={"/images/profile-logo.png"}
@@ -55,6 +58,7 @@ export default function ChatLayout({ children }) {
                   width={40}
                   height={40}
                   className="rounded-full"
+                  alt="Display profile"
                 />
               </div>
               <div>
@@ -69,6 +73,7 @@ export default function ChatLayout({ children }) {
                   width={40}
                   height={40}
                   className="rounded-full"
+                  alt="Display profile"
                 />
               </div>
               <div>
