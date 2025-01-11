98452073f2a45c91621088e1b5e319c68697d360
Soumyadip Dey <soumyadipdey802@gmail.com>
2025-01-11 21:35:34 +0530

    tab bar is being designed

--- client/src/components/shared/sidebar/SidebarWrapper.jsx
+++ client/src/components/shared/sidebar/SidebarWrapper.jsx
@@ -4,9 +4,9 @@ import MobileNav from './navbar/MobileNav'
 
 export default function SidebarWrapper({children}) {
   return (
-    <div className='h-screen w-full p-4 flex flex-col lg:flex-row gap-4'>
+    <div className='h-screen flex-col-reverse w-full p-4 flex lg:flex-row gap-4'>
         <DesktopNav/>
-        {/* <MobileNav/> */}
+        <MobileNav/>
         <main className='h-[calc(100%-80px)] lg:h-full w-full flex gap-4'>
             {children}
         </main>
--- client/src/components/shared/sidebar/navbar/MobileNav.jsx
+++ client/src/components/shared/sidebar/navbar/MobileNav.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+"use client"
+import { Card } from "@/components/ui/card";
+import { useNavigation } from "@/hooks/useNavigation"
 
 export default function MobileNav() {
+  const paths = useNavigation();
   return (
-    <div>MobileNav</div>
+    <Card className="flex lg:hidden justify-center items-center p-4">
+      <nav></nav>
+      <div className="flex flex-col items-centergap-4">
+
+      </div>
+    </Card>
   )
 }
