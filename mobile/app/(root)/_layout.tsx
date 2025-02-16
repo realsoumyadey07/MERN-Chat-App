d01003c1cbaf6d61247ae46994b65b3c37977f04
Soumyadip Dey <soumyadipdey802@gmail.com>
2025-02-16 14:15:14 +0530

    conversation page is getting ready

--- mobile/app/(root)/_layout.tsx
+++ mobile/app/(root)/_layout.tsx
@@ -1,28 +1,31 @@
-import Entypo from "@expo/vector-icons/Entypo";
+import React from "react";
+import { View, Text, TouchableOpacity, Pressable } from "react-native";
 import { Tabs } from "expo-router";
-import { Image, Text, View } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 
-
-interface TabIconInterface {
-  icon: Object;
+interface TabIconProps {
+  iconName: keyof typeof FontAwesome.glyphMap;
   color: string;
-  name: string;
   focused: boolean;
+  name: string;
 }
 
-const TabIcon = ({ icon, color, name, focused }: TabIconInterface) => {
+const TabIcon: React.FC<TabIconProps> = ({
+  iconName,
+  color,
+  focused,
+  name,
+}) => {
   return (
-    <View className="flex-1 justify-center items-center gap-1">
-      <Image
-        source={icon}
-        resizeMode="contain"
-        tintColor={color}
-        className="w-5 h-5"
-      />
+    <View
+      style={{ alignItems: "center", justifyContent: "center" }}
+    >
+      <FontAwesome name={iconName} size={24} color={color} />
       <Text
-        className={`${focused ? "font-psemibol" : "font-pregular"}`}
         style={{
-          color: color,
+          color: focused ? color : "gray",
+          fontSize: 10,
+          marginTop: 4,
         }}
       >
         {name}
@@ -31,35 +34,73 @@ const TabIcon = ({ icon, color, name, focused }: TabIconInterface) => {
   );
 };
 
-const Layout = () => {
+const TabNavigator: React.FC = () => {
   return (
-    <>
-      <Tabs screenOptions={{
-          tabBarShowLabel: false,
-          tabBarActiveTintColor: '#ffa001',
-          tabBarInactiveTintColor: '#cdcde0',
-          tabBarStyle: {
-            backgroundColor: '#fff',
-            borderTopWidth: 1,
-            borderTopColor: "#232533",
-            height: 80,
-          },
-          headerShadowVisible: false
-        }}>
-        <Tabs.Screen
-          name="conversations"
-          options={{
-            title: "conversations",
-            tabBarIcon: ({ color, focused }) => (
-              <TabIcon icon={Entypo} color={color} focused={focused} name="Chats" />
-            ),
-          }}
-        />
-        <Tabs.Screen name="group" />
-        <Tabs.Screen name="user" />
-      </Tabs>
-    </>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: "#141414",
+        tabBarInactiveTintColor: "#807e7e",
+        tabBarStyle: {
+          backgroundColor: "#fff",
+          borderTopWidth: 1,
+          borderTopColor: "#232533",
+          height: 80,
+          paddingBottom: 10,
+          paddingTop: 10,
+          alignItems: "center",
+          justifyContent: "center",
+        },
+        tabBarItemStyle: {
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        },
+        headerShadowVisible: false,
+      }}
+    >
+      <Tabs.Screen
+        name="conversations"
+        options={{
+          title: "Conversations",
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              iconName="comment"
+              color={color}
+              focused={focused}
+              name="Chats"
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="group"
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              iconName="users"
+              color={color}
+              focused={focused}
+              name="Groups"
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="user"
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              iconName="user"
+              color={color}
+              focused={focused}
+              name="Profile"
+            />
+          ),
+        }}
+      />
+    </Tabs>
   );
 };
 
-export default Layout;
+export default TabNavigator;
--- mobile/app/(root)/conversations.tsx
+++ mobile/app/(root)/conversations.tsx
@@ -1,14 +1,71 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { FontAwesome } from "@expo/vector-icons";
 
-const conversations = () => {
+const Conversations = () => {
   return (
-    <View>
-      <Text>conversations</Text>
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <FontAwesome name="search" size={20} color="gray" style={styles.icon} />
+        <TextInput
+          placeholder="Search your chats..."
+          style={styles.input}
+          placeholderTextColor="gray"
+        />
+      </View>
+      <View>
+        <TouchableOpacity style={styles.chatContainer}>
+          {/* <Image source={}/> */}
+          <FontAwesome name="user-circle-o" size={30} color="black" />
+          <View style={styles.messageSection}>
+            <Text>Name</Text>
+            <Text>messages...</Text>
+          </View>
+          <Text>Yesterday</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default conversations
+export default Conversations;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 20,
+    backgroundColor: "#f5f5f5",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.7)",
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginBottom: 20,
+    borderWidth: 1,
+    borderColor: "gray",
+  },
+  icon: {
+    marginRight: 8,
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    color: "black",
+  },
+  chatContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    justifyContent: "space-between",
+    backgroundColor: "#cfcecc",
+    padding: 15,
+    borderRadius: 10
+  },
+  messageSection: {
+    width: "60%"
+  }
+});
