0cdd9f34486e1221443abbc2b9805e0bcfffd7ef
Soumyadip Dey <soumyadipdey802@gmail.com>
2025-05-17 22:13:24 +0530

    origin has changed

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: [process.env.ORIGIN, "http://localhost:3000"],
+        origin: "https://mern-chat-app-six-orpin.vercel.app",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     }
@@ -34,7 +34,7 @@ const io = new Server(server, {
 
 app.set("io", io);
 app.use(cors({
-    origin: [process.env.ORIGIN, "http://localhost:3000"],
+    origin: "https://mern-chat-app-six-orpin.vercel.app",
     credentials: true
 }));
 app.use(express.json({limit: "50mb"}));
