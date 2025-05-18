25751bbb803b5094e85e53034c8d3694e4282526
Soumyadip Dey <soumyadipdey802@gmail.com>
2025-05-18 09:32:39 +0530

    database connection changed

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -34,6 +34,7 @@ userRouter.get("/get-all-requests",IsAuthenticated , getAllRequests);
 userRouter.get("/get-requests-by-name",IsAuthenticated , getRequestsByName);
 userRouter.get("/get-friends",IsAuthenticated , getMyFriends);
 userRouter.get("/get-user-details/:id", IsAuthenticated, getUserDetails);
+// for testing
 userRouter.get("/try", (req, res)=> {
     res.send("Ok tested!");
 });
--- server/utils/db.js
+++ server/utils/db.js
@@ -1,14 +1,12 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 dotenv.config();
-const dbUrl = process.env.DATABASE_URL || "";
+
+const dbUrl = process.env.DATABASE_URL;
 
 export const connectDb = async () => {
   try {
-    const response = await mongoose.connect(dbUrl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const response = await mongoose.connect(dbUrl);
     console.log(`Database is connected to: ${response.connection.host}`);
   } catch (error) {
     console.log(error.message);
